9ae309c91d3d844a9ea65b5e6dedbe56a22c0dbe
Michael Balagna <lunchmeat13@gmail.com>
2017-09-25 14:05:52 -0600

    Add event delegation stuff.

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ $(appReady);
 function appReady() {
   $('.btn').click(function() {
     $('#red').toggleClass("blue")
+  });
+
+
+  $('.red').on("mouseover", function() {
+    console.log("You hovered over stuff in red");
+  })
+
+  $('.addStuff').click(function() {
+      $('.red').append("<p>Stuff</p>")
   })
 }
